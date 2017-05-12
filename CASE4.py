9a0d51d2ceb09e0344c25fd3a5151bb30c737f83
Prateek Chanda <prateekkol21@gmail.com>
2017-05-12 11:08:10 +0530

    Create CASE4.py

---
+++ CASE4.py
@@ -0,0 +1,57 @@
+# First, we import a tool to allow text to pop up on a plot when the cursor
+# hovers over it.  Also, we import a data structure used to store arguments
+# of what to plot in Bokeh.  Finally, we will use numpy for this section as well!
+
+from bokeh.models import HoverTool, ColumnDataSource
+import numpy as np
+
+# Let's plot a simple 5x5 grid of squares, alternating in color as red and blue.
+
+plot_values = [1,2,3,4,5]
+plot_colors = ["red", "blue"]
+
+# How do we tell Bokeh to plot each point in a grid?  Let's use a function that
+# finds each combination of values from 1-5.
+from itertools import product
+
+grid = list(product(plot_values, plot_values))
+print(grid)
+
+# The first value is the x coordinate, and the second value is the y coordinate.
+# Let's store these in separate lists.
+
+xs, ys = zip(*grid)
+print(xs)
+print(ys)
+
+# Now we will make a list of colors, alternating between red and blue.
+
+colors = [plot_colors[i%2] for i in range(len(grid))]
+print(colors)
+
+# Finally, let's determine the strength of transparency (alpha) for each point,
+# where 0 is completely transparent.
+
+alphas = np.linspace(0, 1, len(grid))
+
+# Bokeh likes each of these to be stored in a special dataframe, called
+# ColumnDataSource.  Let's store our coordinates, colors, and alpha values.
+
+source = ColumnDataSource(
+    data={
+        "x": xs,
+        "y": ys,
+        "colors": colors,
+        "alphas": alphas,
+    }
+)
+# We are ready to make our interactive Bokeh plot!
+
+output_file("Basic_Example.html", title="Basic Example")
+fig = figure(tools="resize, hover, save")
+fig.rect("x", "y", 0.9, 0.9, source=source, color="colors",alpha="alphas")
+hover = fig.select(dict(type=HoverTool))
+hover.tooltips = {
+    "Value": "@x, @y",
+    }
+show(fig)

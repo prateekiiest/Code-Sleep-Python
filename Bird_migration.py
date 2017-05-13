b509792faae661e062f8bca989d219027f8a843d
Prateek Chanda <prateekkol21@gmail.com>
2017-05-13 15:35:45 +0530

    Update Bird_migration.py

--- Bird_migration.py
+++ Bird_migration.py
@@ -11,3 +11,41 @@ grouped_birds.head()
 # Assign this to `mean_altitudes`.
 
 mean_altitudes = grouped_birds.altitude.mean()
+
+
+
+
+# Convert birddata.date_time to the `pd.datetime` format.
+birddata.date_time = pd.to_datetime(birddata.date_time)
+
+# Create a new column of day of observation
+birddata["date"] = birddata.date_time.dt.date
+
+# Check the head of the column.
+birddata.date.head()
+
+grouped_bydates = birddata.groupby("date")
+mean_altitudes_perday = grouped_bydates.altitude.mean()
+
+
+
+
+grouped_birdday = birddata.groupby(["bird_name","date"])
+mean_altitudes_perday = grouped_birdday.altitude.mean()
+
+# look at the head of `mean_altitudes_perday`.
+mean_altitudes_perday.head()
+
+
+grouped_birdday = birddata.groupby(["bird_name","date"])
+
+eric_daily_speed  = grouped_birdday.speed_2d.mean()["Eric"]
+sanne_daily_speed = grouped_birdday.speed_2d.mean()["Sanne"]
+
+nico_daily_speed  = grouped_birdday.speed_2d.mean()["Nico"]
+
+eric_daily_speed.plot(label="Eric")
+sanne_daily_speed.plot(label="Sanne")
+nico_daily_speed.plot(label="Nico")
+plt.legend(loc="upper left")
+plt.show()

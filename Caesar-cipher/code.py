06e3988085c61a94e0188de061c6acc03d4acd05
nupurgunwant <nupurgunwant2911@gmail.com>
2017-12-09 21:27:16 +0530

    Made the official caesar cipher that treats ' ' as unchanged string. Added the input feature Removed unnecessary lines

--- Caesar-cipher/code.py
+++ Caesar-cipher/code.py
@@ -2,10 +2,8 @@ import string
 string.ascii_lowercase
 
 # We will consider the alphabet to be these letters, along with a space.
-alphabet = string.ascii_lowercase + " "
 
 # create `letters` here!
-letters = dict(enumerate(alphabet, 0))
 
 
 
@@ -13,24 +11,13 @@ letters = dict(enumerate(alphabet, 0))
 alphabet = string.ascii_lowercase + " "
 letters = dict(enumerate(alphabet))
 
-#key = 3
-encryption_key=3
 # define `coded_message` here!
-encoding= {}
-
-        
-key = (encryption_key)%27
-for let in alphabet:
-        encoding.update({let : key})
-        key =(key + 1)%27
-
-del encoding[' ']
 
 
 
 ##################
 
-message = "hi my name is caesar"
+message = raw_input("Enter a string: ")
 
 def caesar(mee, encryption_key):
         
@@ -47,22 +34,22 @@ def caesar(mee, encryption_key):
    # me = message.replace(" ","")
     # return the encoded message as a single string!
     for l in mee:
-        ty = letters[encoding[l]]
-        encoded_mess = encoded_mess + (ty)
+    	if(l!=' '):
+    		ty = letters[encoding[l]]
+    	    	encoded_mess = encoded_mess + (ty)
+    		    	    
+    	else:
+    		encoded_mess = encoded_mess + ' '
+    			
         
-    
-    
     return encoded_mess
     
     
-encoded_message = (caesar(message,encryption_key = 3))
+encryption_key=input("Enter a number: ")
+    
+encoded_message = (caesar(message,encryption_key))
 print(encoded_message)
 
 
-
-
-
-decoded_message = (caesar(encoded_message,encryption_key=-3))
+decoded_message = (caesar(encoded_message,-1*encryption_key))
 print(decoded_message)
-        
-        

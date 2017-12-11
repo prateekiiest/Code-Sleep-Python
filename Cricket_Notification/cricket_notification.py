aae766e7f5d25d2442fff0553c6dde39fcfd0643
Vivek Gupta <vivekgupta3.vg@gmail.com>
2017-12-11 14:08:12 +0530

    added brackets for py3, removed unnecessary lines

--- Cricket_Notification/cricket_notification.py
+++ Cricket_Notification/cricket_notification.py
@@ -1,107 +1,32 @@
-
-# coding: utf-8
-
-# In[2]:
-
-
-url = "http://cricapi.com/api/matches?apikey=D18haZnedRZ3QijAQEJJV97U5r23"
-
-
-# In[3]:
-
-
 import requests
-
-
-# In[4]:
-
-
 import notify2
-
-
-# In[5]:
-
-
-r = requests.get(url)
-
-
-# In[6]:
-
-
-print(r)
-
-
-# In[7]:
-
-
-print r.text
-
-
-# In[8]:
-
-
 import json
 
-
-# In[10]:
-
+url = "http://cricapi.com/api/matches?apikey=D18haZnedRZ3QijAQEJJV97U5r23"
+r = requests.get(url)
+print(r)
+print(r.text)
 
 data = json.loads(r.text)
-print data
-
-
-# In[11]:
-
-
+print(data)
 print(data['matches'][0])
-
-
-# In[12]:
-
-
 print (data['matches'][0]['team-1'])
-
-
-# In[20]:
-
 
 id=""
 for item in data['matches']:
     if (item['team-1']=="India" or item["team-2"]=="India"):
-        print item
+        print(item)
         if item["matchStarted"]:
             id=item["unique_id"]
-
-
-# In[14]:
-
-
-print id
-
-
-# In[18]:
-
+print(id)
 
 if id:
     r = requests.get("http://cricapi.com/api/cricketScore?apikey=D18haZnedRZ3QijAQEJJV97U5r23&unique_id="+str(id))
     score = json.loads(r.text)
-    print score
-
-
-# In[19]:
-
-
-print score['score'] 
-print score['stat']
-
-
-# In[17]:
-
+    print(score)
+print(score['score'])
+print(score['stat'])
 
 notify2.init("Cric - Notify")
-n = notify2.Notification(score["stat"],
-                         score["score"],
-                         "system"   # Icon name
-                        )
+n = notify2.Notification(score["stat"], score["score"], "system")
 n.show()
-

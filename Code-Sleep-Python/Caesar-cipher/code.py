51b94930fc1c40af11bcf139b6c836b2b0e7ad43
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-07-21 01:14:55 +0530

    Optimized

--- Code-Sleep-Python/Caesar-cipher/code.py
+++ Code-Sleep-Python/Caesar-cipher/code.py
@@ -1,55 +1,23 @@
-import string
-string.ascii_lowercase
-
-# We will consider the alphabet to be these letters, along with a space.
-
-# create `letters` here!
-
-
-
-##
-alphabet = string.ascii_lowercase + " "
-letters = dict(enumerate(alphabet))
-
-# define `coded_message` here!
-
-
-
-##################
-
-message = raw_input("Enter a string: ")
-
-def caesar(mee, encryption_key):
-        
-    encoding= {}
-
-        
-    key = (encryption_key)%27
-    for let in alphabet:
-        encoding.update({let : key})
-        key =(key + 1)%27
-    #del encoding[" "]
-
-    encoded_mess = ""
-   # me = message.replace(" ","")
-    # return the encoded message as a single string!
-    for l in mee:
-    	if(l!=' '):
-    		ty = letters[encoding[l]]
-    	    	encoded_mess = encoded_mess + (ty)
-    		    	    
-    	else:
-    		encoded_mess = encoded_mess + ' '
-    			
-        
-    return encoded_mess
-    
-    
-encryption_key=input("Enter a number: ")
-    
-encoded_message = (caesar(message,encryption_key))
-print(encoded_message)
-
-
-decoded_message = (caesar(encoded_message,-1*encryption_key))
-print(decoded_message)
+def encrypt(text,s): 
+    result = "" 
+  
+    # traverse text 
+    for i in range(len(text)): 
+        char = text[i] 
+  
+        # Encrypt uppercase characters 
+        if (char.isupper()): 
+            result += chr((ord(char) + s-65) % 26 + 65) 
+  
+        # Encrypt lowercase characters 
+        else: 
+            result += chr((ord(char) + s - 97) % 26 + 97) 
+  
+    return result 
+  
+#check the above function 
+text = "ATTACKATONCE"
+s = 4
+print "Text  : " + text 
+print "Shift : " + str(s) 
+print "Cipher: " + encrypt(text,s) 

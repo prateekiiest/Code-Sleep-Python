2e5daec485e820035db319f0242fbe5ec1f843ab
Jui Shah <34505328+shahjui2000@users.noreply.github.com>
2019-10-16 01:17:13 +0530

    Update stt.py

--- Code-Sleep-Python/speech-to-text/stt.py
+++ Code-Sleep-Python/speech-to-text/stt.py
@@ -1,14 +1,17 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
+flag = 1
+while flag=1:
+    
+    with sr.Microphone() as source:
+        print("Say something... ")
+        audio = r.listen(source)
 
-with sr.Microphone() as source:
-    print("Say something... ")
-    audio = r.listen(source)
-
-try:
-    print("Google thinks you said: {}".format(r.recognize_google(audio)))
-except sr.UnkownValueError:
-    print("Couldn't understand")
-except sr.RequestError as e:
-    print("Couldn't request results; {}".format(e))
+    try:
+        print("Google thinks you said: {}".format(r.recognize_google(audio)))
+        flag = 0
+    except sr.UnkownValueError:
+        print("Couldn't understand your voice. Please speak again.")
+    except sr.RequestError as e:
+        print("Couldn't request results. Please speak again.; {}".format(e))

efefe7367f570f93e0be25f41ec5b4438a3e41d0
Zhanet Ivanova <ivanova.zhanet@gmail.com>
2019-10-26 15:52:39 +0100

    Commented out the head() calls as they don't need to run in the script. Clarified how they can be used in ipython in the comments

--- Code-Sleep-Python/Bird_migration/code.py
+++ Code-Sleep-Python/Bird_migration/code.py
@@ -12,14 +12,14 @@ grouped_birds = birddata.groupby("bird_name")
 mean_speeds = grouped_birds.speed_2d.mean()
 
 # The `head` method prints the first 5 lines of each bird.
-grouped_birds.head()
+# This is useful if you are running this in ipython and need to see some data.
+# Call this only on pandas data frame.
+# mean_speeds.head()
 
 # Find the mean `altitude` for each bird.
 # Assign this to `mean_altitudes`.
 
 mean_altitudes = grouped_birds.altitude.mean()
-
-
 
 
 # Convert birddata.date_time to the `pd.datetime` format.
@@ -28,8 +28,8 @@ birddata.date_time = pd.to_datetime(birddata.date_time)
 # Create a new column of day of observation
 birddata["date"] = birddata.date_time.dt.date
 
-# Check the head of the column.
-birddata.date.head()
+# If you're in ipython and want to check the head of the column run:
+# birddata.date.head()
 
 grouped_bydates = birddata.groupby("date")
 mean_altitudes_perday = grouped_bydates.altitude.mean()
@@ -40,8 +40,8 @@ mean_altitudes_perday = grouped_bydates.altitude.mean()
 grouped_birdday = birddata.groupby(["bird_name","date"])
 mean_altitudes_perday = grouped_birdday.altitude.mean()
 
-# look at the head of `mean_altitudes_perday`.
-mean_altitudes_perday.head()
+# If you're in ipython and want to look at the head of `mean_altitudes_perday` run:
+# mean_altitudes_perday.head()
 
 
 grouped_birdday = birddata.groupby(["bird_name","date"])

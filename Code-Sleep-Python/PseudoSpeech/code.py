9022353cbfeff623a80d63b264c27fbbc8b0c098
Gleb <gleb270@mail.ru>
2019-10-27 20:31:06 +0700

    fix PEP

--- Code-Sleep-Python/PseudoSpeech/code.py
+++ Code-Sleep-Python/PseudoSpeech/code.py
@@ -12,19 +12,23 @@ n_min_length_word = 2
 def get_word(n):
     return "".join(random.choices(lower, k=n))
 
+
 def get_length():
     return random.randint(n_min_length_word, n_max_length_word)
+
 
 def get_text():
     out = ""
     for i in range(10):
         out += random.choice(upper) + get_word(get_length())
         word_count = random.randint(1, 10)
-        out += " ".join([get_word(get_length()) for i in range(word_count)])+ ". "
+        words = [get_word(get_length()) for _ in range(word_count)]
+        out += " ".join(words) + ". "
     return out
+
 
 print(get_text())
 
-
-#the output looks like it was crypted with Caesar or other one-to-one substitution cipher
-#so this program could be used for confusing one's minds
+# the output looks like it was crypted with Caesar or other
+# one-to-one substitution cipher,
+# so this program could be used for confusing one's minds

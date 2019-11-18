3699fcc78b66080659d214f1d085f1a08dcfaf55
Zhanet Ivanova <ivanova.zhanet@gmail.com>
2019-11-18 15:56:44 +0000

    Bird migration (#174)
    * Imported required modules and csv data. Added requirements.txt
    
    * Commented out the head() calls as they don't need to run in the script. Clarified how they can be used in ipython in the comments
    
    * Labelled y-axis
    
    * Fixed csv source file URL length issue

--- Code-Sleep-Python/Bird_migration/code.py
+++ Code-Sleep-Python/Bird_migration/code.py
@@ -1,3 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import bird data
+birddata = pd.read_csv('https://d37djvu3ytnwxt.cloudfront.net/assets/'
+'courseware/v1/c72498a54a4513c2eb4ec005adc0010c/'
+'asset-v1:HarvardX+PH526x+3T2016+type@asset+block/bird_tracking.csv')
+
 # First, use `groupby` to group up the data.
 grouped_birds = birddata.groupby("bird_name")
 
@@ -5,14 +14,14 @@ grouped_birds = birddata.groupby("bird_name")
 mean_speeds = grouped_birds.speed_2d.mean()
 
 # The `head` method prints the first 5 lines of each bird.
-grouped_birds.head()
+# This is useful if you are running this in ipython and need to see some data.
+# Call this only on pandas data frame.
+# mean_speeds.head()
 
 # Find the mean `altitude` for each bird.
 # Assign this to `mean_altitudes`.
 
 mean_altitudes = grouped_birds.altitude.mean()
-
-
 
 
 # Convert birddata.date_time to the `pd.datetime` format.
@@ -21,8 +30,8 @@ birddata.date_time = pd.to_datetime(birddata.date_time)
 # Create a new column of day of observation
 birddata["date"] = birddata.date_time.dt.date
 
-# Check the head of the column.
-birddata.date.head()
+# If you're in ipython and want to check the head of the column run:
+# birddata.date.head()
 
 grouped_bydates = birddata.groupby("date")
 mean_altitudes_perday = grouped_bydates.altitude.mean()
@@ -33,8 +42,8 @@ mean_altitudes_perday = grouped_bydates.altitude.mean()
 grouped_birdday = birddata.groupby(["bird_name","date"])
 mean_altitudes_perday = grouped_birdday.altitude.mean()
 
-# look at the head of `mean_altitudes_perday`.
-mean_altitudes_perday.head()
+# If you're in ipython and want to look at the head of `mean_altitudes_perday` run:
+# mean_altitudes_perday.head()
 
 
 grouped_birdday = birddata.groupby(["bird_name","date"])
@@ -48,4 +57,5 @@ eric_daily_speed.plot(label="Eric")
 sanne_daily_speed.plot(label="Sanne")
 nico_daily_speed.plot(label="Nico")
 plt.legend(loc="upper left")
+plt.ylabel("2D mean speed (m/s)")
 plt.show()

96dc0ee001d1bae462db2740b495bf4d13d5ea29
Mahima Chugh <34764608+mahima2112@users.noreply.github.com>
2020-10-03 19:00:11 +0530

    Update cli_github.py

--- Code-Sleep-Python/py_api/cli_github.py
+++ Code-Sleep-Python/py_api/cli_github.py
@@ -8,7 +8,7 @@ data=r.json()
 r=rq.get("https://api.github.com/users/"+user+"/received_events",auth=(user,pwd),params=params)
 data.extend(r.json())
 print("\n")
-length = le(data)
+length = len(data)
 # this way the system wont have to compute length using len() function everytime. it saves on time complexity
 for i in range(length):
     print(data[i]['actor']['display_login'] +" "+ data[i]['type'].replace("Watch","Star").replace("Event","")+" "+ data[i]['repo']['name'][:20] +" at "+ data[i]['repo']['url'].replace("api","www").replace("/repos",""))

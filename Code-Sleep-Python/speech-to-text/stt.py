f6765fe92f2968944dda2bd500e107cf93346d52
Rahul Saini <137695929+ProgramR4732@users.noreply.github.com>
2023-09-02 20:30:54 +0530

    Fixed a bug & updated f-strings.

--- Code-Sleep-Python/speech-to-text/stt.py
+++ Code-Sleep-Python/speech-to-text/stt.py
@@ -2,16 +2,17 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 flag = 1
-while flag=1:
+while flag == 1:
     
     with sr.Microphone() as source:
         print("Say something... ")
         audio = r.listen(source)
 
     try:
-        print("Google thinks you said: {}".format(r.recognize_google(audio)))
+        print(f"Google thinks you said: {r.recognize_google(audio)}")
         flag = 0
     except sr.UnkownValueError:
         print("Couldn't understand your voice. Please speak again.")
     except sr.RequestError as e:
-        print("Couldn't request results. Please speak again.; {}".format(e))
+        print(f"Couldn't request results. Please speak again.; {e}")
+        
